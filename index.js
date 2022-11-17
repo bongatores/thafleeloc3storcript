e8a09b7274e53f7217c9970b5b17df628edfb7a9
henrique <bongador@protonmail.com>
2022-11-16 21:26:03 -0600

    Added web3.storage

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import fs from 'fs';
 import chalk from 'chalk'
 
 const fleekApiKey = process.env.FLEEK_HOST_API;
-const client = new GraphQLClient('https://api.fleek.co/graphql',{
+const fleekClient = new GraphQLClient('https://api.fleek.co/graphql',{
     headers: {
         authorization: fleekApiKey
     }
@@ -15,11 +15,10 @@ const client = new GraphQLClient('https://api.fleek.co/graphql',{
 
 console.log('Initiating IPFS client');
 const ipfs = create();
+const client = new Web3Storage({ token: process.env.WEB3_STORAGE_API })
 
 const main = async () => {
-  if (!fs.existsSync('./car')){
-      fs.mkdirSync('./car');
-  }
+
   const GET_FILES = gql`
     query {
       getSitesByTeam(teamId: "${process.env.FLEEK_TEAM_ID}") {
@@ -33,7 +32,7 @@ const main = async () => {
       }
     }
   `;
-  const data = await client.request(GET_FILES);
+  const data = await fleekClient.request(GET_FILES);
   // Pin locally and at Web3Storage
   for(const site of data.getSitesByTeam.sites){
     const ipfsHash = site.latestDeploy.ipfsHash;
@@ -46,8 +45,19 @@ const main = async () => {
       });
       ipfs.dag.export(ipfsHash).next().then(async item => {
         console.log(chalk.cyan(`Storing ${chalk.green(name)} hash ${ipfsHash} at web3 storage`))
+        console.log(item)
         const car = await CarReader.fromBytes(item.value);
-        console.log(`Sucess ${name}`)
+        const status = await client.status(ipfsHash);
+        if(!status){
+          console.log(car)
+          const cid = await client.putCar(car,{
+            name: name,
+            maxRetries: 3
+          });
+          console.log(`Sucess ${name}`)
+        } else {
+          console.log(`${chalk.cyan(`${name} already in web3 storage`)}`)
+        }
       }).catch(err => {
         console.log(`Error storing ${chalk.cyan(name)} ${chalk.red(err.message)}`)
       })
