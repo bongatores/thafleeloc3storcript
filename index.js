ffa812c6f955cbd4253a45ff025a1e80388699d6
henrique <bongador@protonmail.com>
2022-11-16 22:33:52 -0600

    Added shelljs to do 'ipfs dag export'

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import { CarReader } from '@ipld/car';
 import { GraphQLClient, gql } from 'graphql-request';
 import fs from 'fs';
 import chalk from 'chalk'
+import shell from 'shelljs';
 
 const fleekApiKey = process.env.FLEEK_HOST_API;
 const fleekClient = new GraphQLClient('https://api.fleek.co/graphql',{
@@ -18,7 +19,13 @@ const ipfs = create();
 const client = new Web3Storage({ token: process.env.WEB3_STORAGE_API })
 
 const main = async () => {
-
+  if(!ipfs){
+    console.log(chalk.red("IPFS node needed to run the script"));
+    process.exit();
+  }
+  if (!fs.existsSync('./car')){
+      fs.mkdirSync('./car');
+  }
   const GET_FILES = gql`
     query {
       getSitesByTeam(teamId: "${process.env.FLEEK_TEAM_ID}") {
@@ -40,28 +47,40 @@ const main = async () => {
 
     if(ipfs){
       console.log(`${chalk.cyan("Pining")} ${chalk.green(ipfsHash)}`);
-      ipfs.pin.add(ipfsHash).then(cid => {console.log(`${cid} pinnned!`)}).catch(err => {
-        console.log(err.message)
-      });
-      ipfs.dag.export(ipfsHash).next().then(async item => {
-        console.log(chalk.cyan(`Storing ${chalk.green(name)} hash ${ipfsHash} at web3 storage`))
-        console.log(item)
-        const car = await CarReader.fromBytes(item.value);
-        const status = await client.status(ipfsHash);
-        if(!status){
-          console.log(car)
-          const cid = await client.putCar(car,{
-            name: name,
-            maxRetries: 3
-          });
-          console.log(`Sucess ${name}`)
-        } else {
-          console.log(`${chalk.cyan(`${name} already in web3 storage`)}`)
-        }
-      }).catch(err => {
-        console.log(`Error storing ${chalk.cyan(name)} ${chalk.red(err.message)}`)
-      })
+
+      console.log(`Runing ${chalk.cyan(`ipfs dag export ${ipfsHash} > ./car/${name}.car`)}`)
+      shell.exec(`ipfs dag export ${ipfsHash} > ./car/${name}.car`)
+      console.log(`Sucess exporting as car ${name}`)
     }
   }
+  console.log(chalk.cyan(`Storing at web3 storage`));
+  const files = fs.readdirSync("./car");
+  for(const file of files){
+
+    const inStream = fs.createReadStream(`./car/${file}`);
+    // read and parse the entire stream in one go, this will cache the contents of
+    // the car in memory so is not suitable for large files.
+    console.log(`Uploading ${file} to web3.storage`);
+    console.log(inStream)
+    const car = await CarReader.fromIterable(inStream);
+    console.log(car.cid)
+    const status = await client.status(car.cid);
+    if(!status){
+      console.log(car)
+      const cid = await client.putCar(car,{
+        name: name,
+        maxRetries: 3
+      });
+      console.log(`Sucess ${name}`)
+    } else {
+      console.log(`${chalk.cyan(`${name} already in web3 storage`)}`)
+    }
+    const cid = await client.putCar(car,{
+      name: file,
+      maxRetries: 3
+    });
+    console.log(`${file} cid: ${cid}`);
+  }
+  process.exit();
 }
 main()
